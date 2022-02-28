ad2ea59787de804843fc2189ccd8c6b013d010db
yong <yong9981@gmail.com>
2022-02-27 19:39:32 -0700

    Add NamingConvension, add UUID

--- core/src/main/java/com/github/drinkjava2/jdialects/TableModelUtilsOfEntity.java
+++ core/src/main/java/com/github/drinkjava2/jdialects/TableModelUtilsOfEntity.java
@@ -177,17 +177,25 @@ public abstract class TableModelUtilsOfEntity {// NOSONAR
     private static TableModel entity2ModelIgnoreConfigMethod(Class<?> entityClass) {
         DialectException.assureNotNull(entityClass, "entity2Model method does not accept a null class");
 
-        // Entity
+        // Entity annotation
         String tableName = null;
         Map<String, Object> entityMap = getFirstEntityAnno(entityClass, "Entity");
         tableName = (String) entityMap.get("name");
 
-        // Table
+        // Table annotation
         Map<String, Object> tableMap = getFirstEntityAnno(entityClass, "Table");
         if (!StrUtils.isEmpty(tableMap.get("name")))
             tableName = (String) tableMap.get("name");
-        if (StrUtils.isEmpty(tableName))
-            tableName = entityClass.getSimpleName();
+        
+        if (StrUtils.isEmpty(tableName)) {
+            if (Dialect.globalNamingConversion != null) //by namingConvention
+                tableName = Dialect.globalNamingConversion.getTableName(entityClass);
+            else
+                tableName = entityClass.getSimpleName();
+        }
+        
+        
+        
         TableModel model = new TableModel(tableName); // Build the tableModel
         model.setEntityClass(entityClass);
         if (!tableMap.isEmpty()) {
@@ -321,7 +329,10 @@ public abstract class TableModelUtilsOfEntity {// NOSONAR
                 }
 
                 // @Version annotation
-                ColumnModel col = new ColumnModel(entityfieldName);
+                String columnName=entityfieldName;
+                if(Dialect.globalNamingConversion!=null)
+                    columnName=Dialect.globalNamingConversion.getColumnName(columnName);
+                ColumnModel col = new ColumnModel(columnName);
                 col.entityField(entityfieldName);
                 col.setConverterClassOrName(convertClassOrName);// @Convert's value
 
@@ -415,6 +426,8 @@ public abstract class TableModelUtilsOfEntity {// NOSONAR
                     col.identityId();
                 if (existEntityAnno(field, "TimeStampId"))
                     col.timeStampId();
+                if (existEntityAnno(field, "UUID"))
+                    col.uuid();                
                 if (existEntityAnno(field, "UUID25"))
                     col.uuid25();
                 if (existEntityAnno(field, "UUID26"))
@@ -436,6 +449,8 @@ public abstract class TableModelUtilsOfEntity {// NOSONAR
                             col.autoId();
                         else if ("IDENTITY".equals(strategyStr))
                             col.identityId();
+                        else if ("UUID".equals(strategyStr))
+                            col.uuid();
                         else if ("UUID25".equals(strategyStr))
                             col.uuid25();
                         else if ("UUID26".equals(strategyStr))
